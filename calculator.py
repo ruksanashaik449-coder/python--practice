39bfff3d2193202228fa733d6f492254fd962eda
ruksanashaik449-coder <ruksanashaik449@gmail.com>
2026-07-05 13:36:48 +0530

    added calculator.py

--- calculator.py
+++ calculator.py
@@ -4,17 +4,22 @@ print("1.Addition")
 print("2.Subraction")
 print("3.Multiplication")
 print("4.Division")
-choice=input("Enter your choice:")
+choice=int(input("Enter your choice:"))
 if choice==1:
     result=num1+num2
+    print(result)
 elif choice==2:
     result=num1-num2
+    print(result)
 elif choice==3:
     result=num1*num2
-else:
+    print(result)
+elif choice==4:
     if num2==0:
         print("Cannot divide by 0")
     else:
         result=num1/num2
-print(result)           
+        print(result)  
+else:
+    print(result)         
 
