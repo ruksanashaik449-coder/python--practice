b9830b4e51f4f07881996f367942679b0cf17cf2
ruksanashaik449-coder <ruksanashaik449@gmail.com>
2026-07-05 13:11:44 +0530

    Added student_marks.py

---
+++ student_marks.py
@@ -0,0 +1,13 @@
+marks=int(input("Enter your marks:"))
+if marks>=85:
+    print("Grade A")
+elif marks>=75:
+    print("Grade B")
+elif marks>=65:
+    print("Grade c")
+elif marks>=55:
+    print('Grade D')
+elif marks>=45:
+    print("Grade E")
+else:
+    print("Fail")
